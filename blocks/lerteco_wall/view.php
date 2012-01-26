55d6e7c3deca11ab268bdc6155f1736279939bbb
James Shannon <james@jamesshannon.com>
2012-01-25 17:41:53 -0800

    added type-based CSS class to the <li> in the default view

--- blocks/lerteco_wall/view.php
+++ blocks/lerteco_wall/view.php
@@ -13,12 +13,13 @@
     <ul class="lerteco_wall">
         <?php foreach ($postings as $posting) {
             $user = $posting->getUserInfo();
+            $type = $posting->getType();
             $profile_url = $this->url('/profile','view', $user->getUserID());
         ?>
-            <li>
+            <li class="<?php echo $type->getPackageHandle() . " " . $type->getPackageHandle() . "-" . $type->ptCode ?>">
                 <a href="<?php echo $profile_url ?>"><?php echo $av->outputUserAvatar($user, false, $aspectratio) ?></a>
                 <p class="posting">
-                    <a href="<?php echo $profile_url ?>"><?php echo $posting->getUserInfo()->getUserName() ?></a> <?php echo $wall->getGraffiti($posting->getType()->ptTemplate, $posting->pData, $posting->pCreateDate, false) ?>
+                    <a href="<?php echo $profile_url ?>"><?php echo $posting->getUserInfo()->getUserName() ?></a> <?php echo $wall->getGraffiti($type->ptTemplate, $posting->pData, $posting->pCreateDate, false) ?>
                 </p>
                 <p class="time">
                     <?php echo $date->timeSince(strtotime($posting->pCreateDate)) ?> ago
